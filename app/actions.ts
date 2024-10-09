a42ada1f9cdd6d67ea72ad591d00e6e1da44155b
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-10-09 15:55:21 +0200

    changed urls

--- app/actions.ts
+++ app/actions.ts
@@ -167,8 +167,14 @@ export async function BuyProduct(formData: FormData) {
         destination: data?.User?.connectedAccountId as string,
       },
     },
-    success_url: "http://localhost:3000/payment/success",
-    cancel_url: "http://localhost:3000/payment/cancel",
+    success_url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/payment/success"
+        : "https://marketplacegranaina.vercel.app/payment/success",
+    cancel_url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/payment/cancel"
+        : "https://marketplacegranaina.vercel.app/payment/cancel",
   });
 
   return redirect(session.url as string);
@@ -191,8 +197,14 @@ export async function CreateStripeAccountLink() {
 
   const accountLink = await stripe.accountLinks.create({
     account: data?.connectedAccountId as string,
-    refresh_url: "http://localhost:3000/billing",
-    return_url: `http://localhost:3000/return/${data?.connectedAccountId}`,
+    refresh_url:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/billing"
+        : `https://marketplacegranaina.vercel.app/billing`,
+    return_url:
+      process.env.NODE_ENV === "development"
+        ? `http://localhost:3000/return/${data?.connectedAccountId}`
+        : `https://marketplacegranaina.vercel.app/return/${data?.connectedAccountId}`,
     type: "account_onboarding",
   });
   return redirect(accountLink.url);
